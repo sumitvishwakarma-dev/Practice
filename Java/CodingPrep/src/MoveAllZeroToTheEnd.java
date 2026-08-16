1da3a37c13e251fb309f6134b106cb831c39289e
sumitvishwakarma-dev <sumitvishwakarma.dev@gmail.com>
2026-08-16 23:42:00 +0530

    Added Spring DB security

--- Java/CodingPrep/src/MoveAllZeroToTheEnd.java
+++ Java/CodingPrep/src/MoveAllZeroToTheEnd.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class MoveAllZeroToTheEnd {
 
     public static void main(String[] args) {
--- SpringBoot/SpringSecurity/src/main/java/com/sumitdev/SpringSecurity/repository/RoleRepository.java
+++ SpringBoot/SpringSecurity/src/main/java/com/sumitdev/SpringSecurity/repository/RoleRepository.java
@@ -1,4 +1,14 @@
 package com.sumitdev.SpringSecurity.repository;
 
-public interface RoleRepository {
+import com.sumitdev.SpringSecurity.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    Optional<Role> findByName(String name);
+
 }
