156c6f6b77b7245185680b1adc84d9ca278f0262
sumitvishwakarma-dev <sumitvishwakarma.dev@gmail.com>
2026-08-28 17:04:55 +0530

    Sum of odd number square with List

--- Java/CodingPrep/src/SumOfSqaureOfOddNumbers.java
+++ Java/CodingPrep/src/SumOfSqaureOfOddNumbers.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class SumOfSqaureOfOddNumbers {
 
@@ -12,9 +13,28 @@ public class SumOfSqaureOfOddNumbers {
     public static long sumOfSqaureOfOdd(int [] arrs){
 
         long  sumOfSquareOdd = Arrays.stream(arrs)
-                .filter(x -> x %2 != 0)
+                .filter(x -> x % 2 != 0)
                 .map(x -> x*x)
                 .sum();
+
+        List<Integer> list = Arrays.asList(1,3,2,4);
+        int num = list.stream()
+                .filter(x -> x % 2 != 0)
+                .mapToInt(x -> x * x)
+                .sum();
+        System.out.println(num);
+
+        List<Integer> listInteger = Arrays.stream(arrs)
+                .filter(x -> x % 2 != 0)
+                .boxed()
+                .map(x -> x * x)
+                .toList();
+
+        int sumInteger = 0;
+        for (int i : listInteger){
+            sumInteger += i;
+        }
+        System.out.print(sumInteger);
 
 
         return sumOfSquareOdd;
