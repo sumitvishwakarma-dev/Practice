6332f6e223b802cc7b470ee23c89c2b072080a36
sumitvishwakarma-dev <sumitvishwakarma.dev@gmail.com>
2026-09-05 13:13:27 +0530

    Count frequency Program added to show duplicates

--- Java/CodingPrep/src/CountFrequencyWithStream.java
+++ Java/CodingPrep/src/CountFrequencyWithStream.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -8,10 +9,14 @@ public class CountFrequencyWithStream {
     public static void main(String [] args){
         int [] arr ={2,1,4,3,3,2,9,6,9,4,7,5,8};
         Map<Integer,Long> countFrequency = countFrequency(arr);
+        System.out.println("Frequency Count are :: ");
+
         System.out.println(countFrequency);
     }
 
     public static Map<Integer,Long> countFrequency(int [] nums){
+
+        // ----------- With Stream frequency Count ------------
 
         Map<Integer,Long> countFrequencyMap = Arrays.stream(nums)
                 .boxed()
@@ -29,10 +34,24 @@ public class CountFrequencyWithStream {
 //                    countFrequencyMap.containsKey(i) ? countFrequencyMap.get(i)+1 : 1);
 //        }
 
-        countFrequencyMap.entrySet()
+        // ----------- Only Taking count duplicates ------------
+
+//        countFrequencyMap.entrySet()
+//                .stream()
+//                .filter(element -> element.getValue() > 1)
+//                .forEach(System.out :: println);
+
+        // ----------- Only duplicates Key print ------------
+
+        System.out.println("Only duplicates Key print :: ");
+        List<Integer> list =  countFrequencyMap.entrySet()
                 .stream()
                 .filter(element -> element.getValue() > 1)
-                .forEach(System.out :: println);
+                .map(Map.Entry :: getKey)
+             //  .forEach(System.out:: println);
+
+               .toList();
+                System.out.println(list);
 
         return countFrequencyMap;
     }
