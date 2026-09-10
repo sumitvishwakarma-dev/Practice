88f13da3862b626f5e43966cf99f4d1c2763d749
sumitvishwakarma-dev <sumitvishwakarma.dev@gmail.com>
2026-09-10 23:27:10 +0530

    Singleton Pattern

---
+++ ConceptBuilding/src/SingletonPattern.java
@@ -0,0 +1,20 @@
+public class SingletonPattern {
+
+    private static volatile SingletonPattern instance;
+
+    private SingletonPattern(){ } //Lazy Loading
+
+    public static SingletonPattern getInstance(){
+
+        if(instance == null){
+            synchronized (SingletonPattern.class){
+                if(instance == null){
+                    instance = new SingletonPattern();
+                }
+            }
+        }
+        return instance;
+
+    }
+
+}
